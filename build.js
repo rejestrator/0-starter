be734f36207f5a1cc6a0380a7c3a0abc00e95f9d
rejestrator <rejestrator.mily@gmail.com>
2021-11-03 16:29:50 -0400

    initial commit

---
+++ build.js
@@ -0,0 +1,43 @@
+const metadataOpt = {
+  site: {
+    lang: 'en',
+    author: 'Adam Surdyk'
+  }
+}
+
+const markdownOpt = {}
+
+const layoutsOpt = {
+  default: 'default.pug',
+  directory: 'layouts'
+}
+
+const componentsOpt = {
+  'componentDirectory': 'node_modules',
+  'components': {
+    'bootstrap': {
+      'dist/css/bootstrap.min.css': 'css'
+    }
+  }
+}
+
+const beautifyOpt = {}
+
+const Metalsmith      = require('metalsmith')(__dirname);
+const debugUi         = require('metalsmith-debug-ui').report;
+const markdown        = require('metalsmith-markdown')(markdownOpt);
+const layouts         = require('metalsmith-layouts')(layoutsOpt);
+const beautify        = require('metalsmith-beautify')(beautifyOpt);
+const components      = require('metalsmith-components')(componentsOpt);
+
+Metalsmith
+  .clean(false).destination('./out').use(debugUi('Init'))
+  .metadata(metadataOpt)            .use(debugUi('Native Metadata'))
+  .use(components)                  .use(debugUi('Components'))
+  .use(markdown)                    .use(debugUi('Markdown'))
+  .use(layouts)                     .use(debugUi('Layouts'))
+  .use(beautify)                    .use(debugUi('Beautify'))
+  .build(err => {
+    if(err) console.log(err);
+    console.log('Rendering Metalsmith complited');
+  })
---
+++ src/index.md
@@ -0,0 +1,12 @@
+---
+createdAt: 11/3/2021, 12:09:17 PM
+updatedAt: 11/3/2021, 12:09:32 PM
+publish: draft
+lang: en
+author: Adam Surdyk
+title: Simple metalsmith starter
+description:
+excerpt:
+tags:
+---
+Consequat incididunt nisi aute laboris amet do nisi et anim et proident amet. Labore occaecat id ex velit nisi adipisicing consectetur cillum veniam commodo aute consequat.
